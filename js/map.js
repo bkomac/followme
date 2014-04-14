200149ef2858a1a53c6dc3faa1aec38bfc2666ed
bkomac <b@komac.si>
2014-04-14 21:48:01 +0200

    revert to...

--- js/map.js
+++ js/map.js
@@ -1,78 +1,30 @@
-var map;
-var marker;
-var infowindow;
-var watchID;
-
-
-// prva beforeShow
-$("#prva").on("beforepageshow", function(e) {
-
-	 google.load("maps", "3.8", {"callback": map, other_params: "sensor=true&language=en"});
-	
-	
-	
+$( document ).on( "pageinit", "#map-page", function() {
+    var defaultLatLng = new google.maps.LatLng(34.0983425, -118.3267434);  // Default to Hollywood, CA when no geolocation support
+    if ( navigator.geolocation ) {
+        function success(pos) {
+            // Location found, show map with these coordinates
+            drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        }
+        function fail(error) {
+            drawMap(defaultLatLng);  // Failed to find location, show default map
+        }
+        // Find the users current position.  Cache the location for 5 minutes, timeout after 6 seconds
+        navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 500000, enableHighAccuracy:true, timeout: 6000});
+    } else {
+        drawMap(defaultLatLng);  // No geolocation support, show default map
+    }
+    function drawMap(latlng) {
+        var myOptions = {
+            zoom: 10,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+        // Add an overlay to the map of current lat/lng
+        var marker = new google.maps.Marker({
+            position: latlng,
+            map: map,
+            title: "Greetings!"
+        });
+    }
 });
-
-//private
-
-function map(){
-    var latlng = new google.maps.LatLng(55.17, 23.76);
-    var myOptions = {
-      zoom: 6,
-      center: latlng,
-      streetViewControl: true,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      zoomControl: true
-    };
-    map = new google.maps.Map(document.getElementById("map"), myOptions);
-     
-    google.maps.event.addListenerOnce(map, 'tilesloaded', function(){
-        //get geoposition once
-        //navigator.geolocation.getCurrentPosition(geo_success, geo_error, { maximumAge: 5000, timeout: 5000, enableHighAccuracy: true });
-        //watch for geoposition change
-        watchID = navigator.geolocation.watchPosition(geo_success, geo_error, { maximumAge: 5000, timeout: 5000, enableHighAccuracy: true });  
-    });
-}
-
-function geo_error(error){
-    //comment
-    alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
-}
- 
-function geo_success(position) {
-     
-    map.setCenter(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
-    map.setZoom(15);
- 
-    var info =
-    ('Latitude: '         + position.coords.latitude          + '<br>' +
-    'Longitude: '         + position.coords.longitude         + '<br>' +
-    'Altitude: '          + position.coords.altitude          + '<br>' +
-    'Accuracy: '          + position.coords.accuracy          + '<br>' +
-    'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '<br>' +
-    'Heading: '           + position.coords.heading           + '<br>' +
-    'Speed: '             + position.coords.speed             + '<br>' +
-    'Timestamp: '         + new Date(position.timestamp));
- 
-    var point = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    if(!marker){
-        //create marker
-        marker = new google.maps.Marker({
-            position: point,
-            map: map
-        });
-    }else{
-        //move marker to new position
-        marker.setPosition(point);
-    }
-    if(!infowindow){
-        infowindow = new google.maps.InfoWindow({
-            content: info
-        });
-    }else{
-        infowindow.setContent(info);
-    }
-    google.maps.event.addListener(marker, 'click', function() {
-      infowindow.open(map,marker);
-    });
-}
